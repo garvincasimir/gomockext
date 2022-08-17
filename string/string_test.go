b522a6a512a08b0c81d503477c199bd3af0906e0
Garvin Casimir <3106023+garvincasimir@users.noreply.github.com>
2022-08-17 05:29:33 +0000

    add string tests

---
+++ string/string_test.go
@@ -0,0 +1,46 @@
+package stringext
+
+import "testing"
+
+func TestStartsWithFalseWithNoMatch(t *testing.T) {
+	matcher := StartsWith("abc", "Starts with abc")
+	if matcher.Matches("def") {
+		t.Error("def does not start with abc")
+	}
+}
+
+func TestStartsWithTrueWithMatch(t *testing.T) {
+	matcher := StartsWith("abc", "Starts with abc")
+	if !matcher.Matches("abcdef") {
+		t.Error("abcdef does start with abc")
+	}
+}
+
+func TestStartsWithFalseWithNoneString(t *testing.T) {
+	matcher := StartsWith("abc", "Starts with abc")
+	if matcher.Matches(823) {
+		t.Error("Should not match on non string")
+	} 
+}
+
+
+func TestEndsWithFalseWithNoMatch(t *testing.T) {
+	matcher := EndsWith("abc", "Ends with abc")
+	if matcher.Matches("def") {
+		t.Error("def does not end with abc")
+	}
+}
+
+func TestEndsWithTrueWithMatch(t *testing.T) {
+	matcher := EndsWith("def", "Ends with abc")
+	if !matcher.Matches("abcdef") {
+		t.Error("abcdef does end with def")
+	}
+}
+
+func TestEndsWithFalseWithNoneString(t *testing.T) {
+	matcher := EndsWith("abc", "Ends with abc")
+	if matcher.Matches(823) {
+		t.Error("Should not match on non string")
+	} 
+}
